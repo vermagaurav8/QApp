8c44be92ba27b5fc4ae15fda4942881bd0692518
Gaurav Verma <heygaurav8@gmail.com>
2024-01-17 18:31:35 +0530

    Simple WS backend

---
+++ server/server.js
@@ -0,0 +1,50 @@
+/**
+ *  Server entry-point
+ *  @author Gaurav Verma
+ *  @description Listens for WS connections
+ */
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// allowing requests from cross origin
+app.use(cors({origin: '*'}));
+
+// Event Listner for incoming WS Connections 
+io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    socket.on('login', (data) => {
+        if(validateToken(data.token)) {
+            console.log('Logged in');
+            socket.emit('redirect', '/dashboard');
+        } else {
+            socket.emit('redirect','/404');
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+    });
+
+});
+
+const validateToken = (token) => {
+    if(token === 'admin') {
+        return true;
+    }
+    return false;
+}
+
+// Configuring server to listen on port 3000 or env port.
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server listening on PORT ${3000}`);
+})
+
